801a763dd57ba760c700797d7f8af43fb9e60d8b
leon <leonlin6@hotmail.com>
2022-11-01 10:40:20 +0800

    調整API ID連到測試機

--- src/APIs/APIs.js
+++ src/APIs/APIs.js
@@ -1,1 +1,2 @@
-export const ENDPOINT_BASE_URL = 'http://192.168.50.169:5001';
+// export const ENDPOINT_BASE_URL = 'http://192.168.50.169:5001';
+export const ENDPOINT_BASE_URL = 'https://cred-api.snowbridge.tw';
